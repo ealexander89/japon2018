972f9cc1f9900e5384a805dfdebf19fce41786ed
Alex <ealexander@Ericks-MacBook-Pro.local>
2018-02-23 01:22:37 -0600

    doing tests

--- functions.js
+++ functions.js
@@ -1,6 +1,10 @@
-$( document ).ready(function() {
-    $.getJSON("https://ealexander89.github.io/japon2018/itinerary.json", function( data ) {
-        var items = [];
-        console.log(data);
-    });
-});
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        console.log(myObj);
+        
+    }
+};
+xmlhttp.open("GET", "https://ealexander89.github.io/japon2018/itinerary.json", true);
+xmlhttp.send();
