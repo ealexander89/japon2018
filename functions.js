daee9f64a7d68b015b18a360ffc433ceafecc933
Alex <ealexander@Ericks-MacBook-Pro.local>
2018-02-23 01:27:39 -0600

    doing tests

--- functions.js
+++ functions.js
@@ -1,9 +1,14 @@
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        console.log(myObj);
-        
+        var itinerary = JSON.parse(this.responseText);
+
+        if( $(".day") > 0) {
+            var day = itinerary[$(".day").attr('id')];
+
+            console.log(day);
+            
+        }
     }
 };
 xmlhttp.open("GET", "https://ealexander89.github.io/japon2018/itinerary.json", true);
