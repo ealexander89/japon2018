4e95fb539db7505f5181cd02816bc71a4471ce3a
Alex <ealexander@Ericks-MacBook-Pro.local>
2018-02-24 19:32:14 -0600

    doing tests

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+var planIcons = {
+    plane: "airplanemode_active"
+}
+
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -9,7 +13,18 @@ xmlhttp.onreadystatechange = function() {
             console.log("es un dia");
             var day = itinerary[$(".day").attr('id')];
 
-            console.log(day);
+            $('.main-header .title').text(day['day-name']);
+            $('.main-header .sub-title').text(day['place']);
+
+            $.each( day['plan'] , function( index, plan ) {
+                var planHtml = '<div class="plan type-'+ plan.type +'">' +
+                    '<span class="plan-icon">' +
+                        '<i class="material-icons">' + planIcons[plan.type] + '</i>' +
+                    '</span>' +
+                '</div>';
+
+                $(".plan-list").append( planHtml );
+            });
             
         }
     }
